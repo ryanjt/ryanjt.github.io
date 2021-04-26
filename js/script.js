b0f7560684375c499a1f3568222fb3594569768c
Ryan <40405884@live.napier.ac.uk>
2021-04-26 23:56:39 +0100

    v1 - 2021 edition.

---
+++ js/script.js
@@ -0,0 +1,47 @@
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    scroll = function(c, a, b, i) {
+            i++;
+            if (i > 30) return;
+            c.scrollTop = a + (b - a) / 30 * i;
+            setTimeout(function() { scroll(c, a, b, i); }, 20);
+        }
+        // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
+
+$(document).ready(function() {
+
+    getProjects().then(response => {
+        $.each(response, function(i) {
+            var templateString = '<div class="column"><div class="card"><h2>' + response[i].name + '</h2><p>' + response[i].description + '</p><a href="' + response[i].html_url + '"><button type="button" href="#skills" class="btn btn-primary moveBut btn-lg">View Project <i>→</i></button></a></div></div>';
+            $('#test12').append(templateString);
+        })
+    })
+
+
+
+
+
+});
+
+
+async function getProjects() {
+    const response = fetch("https://api.github.com/users/ryanjt/repos")
+        .then(x => x.text())
+        .then(y => projects = JSON.parse(y));
+
+    return response;
+}
